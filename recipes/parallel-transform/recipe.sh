3c9ca62deb7445fc5720e21e09824738983180c6
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2026-01-13 10:48:41 -0800

    update output file name
    update output file name

--- recipes/parallel-transform/recipe.sh
+++ recipes/parallel-transform/recipe.sh
@@ -5,4 +5,4 @@
 
 ogr2ogr -f GPKG -t_srs EPSG:4326 $OUTPUT_FILE $INPUT_FILE && echo "Reprojected $INPUT_FILE to EPSG:4326"
 
-ogr2ogr -f GeoJSON $OUTPUT_FILE $INPUT_FILE && echo "Converted $INPUT_FILE to GeoJSON"
+ogr2ogr -f GeoJSON $(basename $INPUT_FILE .gpkg).geojson $INPUT_FILE && echo "Converted $INPUT_FILE to GeoJSON"
