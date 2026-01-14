a077305828b9603e4ec0fd5a9c0f086db6b6e394
Rushiraj Nenuji <19696935+rushirajnenuji@users.noreply.github.com>
2026-01-14 08:53:19 -0800

    Update recipe
    Update recipe

--- recipes/parallel-transform/recipe.sh
+++ recipes/parallel-transform/recipe.sh
@@ -3,6 +3,6 @@
 # Parallel GeoPackage processing - Reproject and Convert
 # Each partition processes its assigned files independently
 
-ogr2ogr -f GPKG -t_srs EPSG:4326 $OUTPUT_DIR/$(basename $INPUT_FILE .gpkg)_4326.gpkg $INPUT_FILE
+ogr2ogr -f GPKG -t_srs EPSG:4326 "$OUTPUT_DIR"/"$(basename "$INPUT_FILE" .gpkg)_4326.gpkg" "$INPUT_FILE"
 
-ogr2ogr -f GeoJSON $OUTPUT_DIR/$(basename $INPUT_FILE .gpkg).geojson $INPUT_FILE
+ogr2ogr -f GeoJSON "$OUTPUT_DIR"/"$(basename "$INPUT_FILE" .gpkg).geojson" "$INPUT_FILE"
