5d8ff7579a7a500c595751d412624a81140f094c
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2026-01-15 14:06:00 -0800

    update

--- recipes/dai-arcticdem-restructuring/recipe.sh
+++ recipes/dai-arcticdem-restructuring/recipe.sh
@@ -2,4 +2,4 @@
 
 DEST=/var/data/10.18739/A22805140/arcticdem_wmts/jump/ArcticDEM/1/; \
 tr -d '\r' < $INPUT_FILE | awk -F',' 'NR==1{for(i=1;i<=NF;i++){gsub(/^[ "]+|[ "]+$/,"",$i); if($i=="file_path")f=i; if($i=="X_2")x=i; if($i=="Y_2")y=i} next} {print $f "\t" $x "\t" $y}' | \
-while IFS=$'\t' read -r src X2 Y2; do echo "ln '$src' '$DEST/$X2/$Y2.tif'" >> output.log; done
+while IFS=$'\t' read -r src X2 Y2; do echo "ln '$src' '$DEST/$X2/$Y2.tif'" >> $OUTPUT_FILE; done
